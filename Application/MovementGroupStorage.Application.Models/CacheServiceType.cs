9d96ea871d4795fa59b95ca7dbb0b61526c8ebec
Aaron Urkin <a.urkin@gmail.com>
2026-02-18 20:58:23 +0200

    SDCS has been implemented.

---
+++ Application/MovementGroupStorage.Application.Models/CacheServiceType.cs
@@ -0,0 +1,20 @@
+﻿namespace MovementGroupStorage.Application.Models
+{
+    public enum CacheServiceType
+    {
+        /// <summary>
+        /// Unknown type.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Distributed cache service.
+        /// </summary>
+        Distributed = 1,
+
+        /// <summary>
+        /// Memory cache service.
+        /// </summary>
+        Memory = 2,
+    }
+}
