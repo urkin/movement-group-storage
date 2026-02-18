01044a5827d050b27322b6b8757bdbadc5c70431
Aaron Urkin <a.urkin@gmail.com>
2026-02-18 22:09:21 +0200

    Redis `ICacheService` has been implemented.

---
+++ Infrastructure/MovementGroupStorage.Infrastructure.Application.Services/CacheServices/RedisDistributedCacheService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using MovementGroupStorage.Application.Models;
+using MovementGroupStorage.Application.Services;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace MovementGroupStorage.Infrastructure.Application.Services
+{
+    public class RedisDistributedCacheService : ICacheService<string>
+    {
+        private readonly IDatabase _database;
+        private readonly IDistributedCache _cache;
+
+        public RedisDistributedCacheService(IDistributedCache cache, IConnectionMultiplexer redis)
+        {
+            ArgumentNullException.ThrowIfNull(redis);
+
+            _database = redis.GetDatabase();
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        /// <inheritdoc/>
+        public int Count => (int?)_database.Execute("DBSIZE") ?? 0;
+
+        /// <inheritdoc/>
+        public CacheServiceType Type => CacheServiceType.Memory;
+
+        /// <inheritdoc/>
+        public async Task<ApplicationServiceResult> RemoveAsync(string key)
+        {
+            await _cache.RemoveAsync(key);
+            return new ApplicationServiceResult(ApplicationServiceResultStatus.Succeeded);
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApplicationServiceResult> SetAsync<TValue>(string key, TValue value)
+        {
+            var bytes = JsonSerializer.SerializeToUtf8Bytes(value);
+            await _cache.SetAsync(key, bytes);
+            return new ApplicationServiceResult(ApplicationServiceResultStatus.Succeeded);
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApplicationServiceResult> GetAsync<TValue>(string key)
+        {
+            var bytes = await _cache.GetAsync(key);
+            var value = JsonSerializer.Deserialize<TValue>(bytes);
+            return new ApplicationServiceResult(value, ApplicationServiceResultStatus.Succeeded);
+        }
+    }
+}
---
+++ MovementGroupStorage.API/Startup/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using MovementGroupStorage.Application.Models;
+using MovementGroupStorage.Application.Services;
+using MovementGroupStorage.Infrastructure.Application.Services;
+using StackExchange.Redis;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCacheServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMemoryCache();
+
+            services.AddSingleton(
+                typeof(ICacheService<>),
+                typeof(CapacityTtlMemoryCacheService<>));
+
+            var redisConection = configuration
+                .GetSection("Redis")
+                .GetValue<string>("ConnectionString")!;
+            var multiplexer = ConnectionMultiplexer.Connect(redisConection);
+
+            services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.InstanceName = "MovementGroupStorage:";
+                options.ConnectionMultiplexerFactory =
+                    () => Task.FromResult<IConnectionMultiplexer>(multiplexer);
+            });
+
+            services.AddSingleton<ICacheService<string>, RedisDistributedCacheService>();
+
+            services.AddOptions<CapacityTtlCacheServiceOptions>()
+                .BindConfiguration("CapacityTtlCacheServiceOptions")
+                .ValidateOnStart();
+
+            return services;
+        }
+    }
+}
